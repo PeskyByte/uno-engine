f1e6dd82f2d1f65c6986fcd102abc8273d4a28d7
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-09-15 17:31:48 +0300

    Add custom game implementation

--- src/main/java/com/engine/CustomDealer.java
+++ src/main/java/com/engine/CustomDealer.java
@@ -1,2 +1,18 @@
-package com.engine;public class CustomDealer {
+package com.engine;
+
+import com.engine.game.Deck;
+import com.engine.game.ICardDealer;
+import com.engine.player.Player;
+
+import java.util.List;
+
+public class CustomDealer implements ICardDealer {
+    @Override
+    public void deal(List<Player> players, Deck pile) {
+        for(Player player: players){
+            for (int i = 0; i < 3; i++) {
+                player.addCardToHand(pile.drawCardFromTop());
+            }
+        }
+    }
 }
--- src/main/java/com/engine/CustomGame.java
+++ src/main/java/com/engine/CustomGame.java
@@ -1,2 +1,26 @@
-package com.engine;public class CustomGame {
+package com.engine;
+
+import com.engine.game.DefaultDealer;
+import com.engine.game.DefaultGenerator;
+import com.engine.game.Game;
+
+public class CustomGame extends Game {
+
+    private static Game gameSingleton = new CustomGame();
+
+    private CustomGame(){
+
+    }
+
+    public static Game getInstance(){
+        return gameSingleton;
+    }
+
+    @Override
+    public void initialize() {
+        createPlayers();
+        generateDeck(new DefaultGenerator());
+        drawPile.shuffle();
+        dealCards(new CustomDealer());
+    }
 }
