490381abf85179f5cfa7af71ef2d22f1ab56cc53
PeskyByte <hashem.aljarrah.ha@gmail.com>
2024-09-15 17:29:56 +0300

    Add player implementation

--- src/main/java/com/engine/player/Player.java
+++ src/main/java/com/engine/player/Player.java
@@ -1,2 +1,38 @@
-package com.engine.player;public class Player {
+package com.engine.player;
+
+import com.engine.card.Card;
+import com.engine.game.Deck;
+
+public class Player {
+    private final String name;
+    private int score;
+    private final Deck hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        hand = new Deck();
+    }
+
+    public Deck getHand() {
+        return hand;
+    }
+
+    public void addCardToHand(Card card) {
+        hand.addCard(card);
+    }
+
+    public int numberOfCardsLeft() {
+        return hand.getDeckSize();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", score: " + score;
+    }
+
 }
